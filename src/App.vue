5d7799965f1d6825c2a05d134df59ce94f8cd536
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-04-13 16:59:08 +0200

    add  comments

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
+<!-- script js -->
 <script >
 </script>
+<!-- #end script js -->
 
+<!-- template html -->
 <template>
+<!-- header whit nav bar -->
 <header>
   <div class="container">
     <div class="d-flex justify-content-between align-items-center">
@@ -23,54 +27,50 @@
     </div>
   </div>
 </header>
-
+<!-- #end header whit nav bar -->
+<!-- main whit coming soon content -->
 <main>
   <!-- app main whit all content coming soon -->
   <div class="app_main d-flex justify-content-between align-items-center">
     <h1>More coming soon</h1>
   </div>
   <!-- #End app main whit all content coming soon -->
-
   <!-- under main whit all content -->
   <div class=" blue_container d-flex justify-content-between align-items-center">
-
     <div>
     <img src="./assets/img/buy-comics-digital-comics.png" alt="">
       <a href="">DIGITAL COMICS</a>
     </div>
-    
     <div>
       <img src="./assets/img/buy-comics-merchandise.png" alt="">
       <a href="">DC MERCHANDISE</a>
     </div>
- 
     <div>
       <img src="./assets/img/buy-comics-subscriptions.png" alt="">
       <a href="">COMIC SHOP LOCATOR</a>
     </div>
-   
     <div>
       <img src="./assets/img/buy-comics-shop-locator.png" alt="">
       <a href="">SUBSCRIPTION</a>
     </div>
-
     <div>
       <img src="./assets/img/buy-dc-power-visa.svg" alt="">
       <a href="">DC POWER E VISA</a>
     </div>
-  
   </div>
   <!-- #End under main whit all content -->
-
 </main>
-
+<!-- #end main whit coming soon content -->
+<!-- footer  -->
 <footer>
 
 </footer>
+<!-- #end footer  -->
 
 </template>
+<!-- #End template html -->
 
-
+<!-- style css -->
 <style >
 /* header region */
 .header{
@@ -103,4 +103,6 @@ a{
   color: white;
   text-decoration: none;
 }
+
 </style>
+<!-- #end style css -->

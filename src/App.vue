a3b8abb9852a3928050787bca54c871e117720e3
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-04-13 16:55:01 +0200

    add main whit coming soon

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,43 @@
 </header>
 
 <main>
-  <div class="container">
-  <div class="row">
-    <div class="col">
-
-    </div>
+  <!-- app main whit all content coming soon -->
+  <div class="app_main d-flex justify-content-between align-items-center">
+    <h1>More coming soon</h1>
   </div>
-</div>
+  <!-- #End app main whit all content coming soon -->
+
+  <!-- under main whit all content -->
+  <div class=" blue_container d-flex justify-content-between align-items-center">
+
+    <div>
+    <img src="./assets/img/buy-comics-digital-comics.png" alt="">
+      <a href="">DIGITAL COMICS</a>
+    </div>
+    
+    <div>
+      <img src="./assets/img/buy-comics-merchandise.png" alt="">
+      <a href="">DC MERCHANDISE</a>
+    </div>
+ 
+    <div>
+      <img src="./assets/img/buy-comics-subscriptions.png" alt="">
+      <a href="">COMIC SHOP LOCATOR</a>
+    </div>
+   
+    <div>
+      <img src="./assets/img/buy-comics-shop-locator.png" alt="">
+      <a href="">SUBSCRIPTION</a>
+    </div>
+
+    <div>
+      <img src="./assets/img/buy-dc-power-visa.svg" alt="">
+      <a href="">DC POWER E VISA</a>
+    </div>
+  
+  </div>
+  <!-- #End under main whit all content -->
+
 </main>
 
 <footer>
@@ -42,10 +72,11 @@
 
 
 <style >
+/* header region */
 .header{
   height: 300px;
 }
-a{
+nav > a{
   text-decoration: none;
   color: black;
   padding: 0rem 1rem;
@@ -53,5 +84,23 @@ a{
 .c_blue{
   color: blue;
 }
+/* # End header region */
 
+/* Main region  */
+.app_main{
+  height: 250px;
+  background-color: black;
+  color: white;
+}
+/* #End Main region  */
+
+.blue_container{
+  background-color: blue;
+  height: 300px;
+}
+
+a{
+  color: white;
+  text-decoration: none;
+}
 </style>
